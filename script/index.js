1ac3262782952f4dad738b0742973f66bd6c7a26
Samarjit Debnath <samarjit_debnath@Samarjits-MacBook-Air.local>
2023-02-03 15:15:59 +0530

    updating feature

---
+++ script/index.js
@@ -0,0 +1,42 @@
+function PlaySoundHome() {
+    var sound = document.getElementById("home_audio");
+    sound.play();
+}
+
+function PlaySoundAbout() {
+    var sound = document.getElementById("about_audio");
+    sound.play();
+}
+
+function PlaySoundExperience() {
+    var sound = document.getElementById("experience_audio");
+    sound.play();
+}
+
+function PlaySoundProjects() {
+    var sound = document.getElementById("projects_audio");
+    sound.play();
+}
+
+function PlaySoundSkills() {
+    var sound = document.getElementById("skills_audio");
+    sound.play();
+}
+function PlaySoundKnowMore() {
+    var sound = document.getElementById("knowmore_audio");
+    sound.play();
+}
+function PlaySoundNotification() {
+    var sound = document.getElementById("notification_audio");
+    sound.play();
+}
+
+function PlaySoundHelloSamarjit() {
+    var sound = document.getElementById("hello_samarjit_audio");
+    sound.play();
+}
+
+function PlaySoundSamarjit() {
+    var sound = document.getElementById("samarjit_audio");
+    sound.play();
+}
